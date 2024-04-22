e6e6f59d6738f5b9c311713bda1b3f5a43aa3a14
Charles Chitty <charlescchitty@gmail.com>
2024-04-21 19:33:09 -0700

    Implemented base users routes

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import app from app.js;
+
+
+// GET Route for homepage
+
+// GET Route for Login
+// POST Route for Login
+// GET Route for a game
+// WGET Route for editing a porfile
+// POST Route for editing a profile
